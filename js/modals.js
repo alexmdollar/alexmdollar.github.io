675aa149a84526ee1e1099e1bbea85b16aea5ff3
alexmdollar <42846394+alexmdollar@users.noreply.github.com>
2025-01-15 17:53:46 -0500

    Add files via upload

---
+++ js/modals.js
@@ -0,0 +1,92 @@
+const open1 = document.getElementById('open1');
+const one = document.getElementById('one');
+const close1 = document.getElementById('close1');
+const open2 = document.getElementById('open2');
+const two = document.getElementById('two');
+const close2 = document.getElementById('close2');
+const open3 = document.getElementById('open3');
+const three = document.getElementById('three');
+const close3 = document.getElementById('close3');
+const open4 = document.getElementById('open4');
+const four = document.getElementById('four');
+const close4 = document.getElementById('close4');
+const open5 = document.getElementById('open5');
+const five = document.getElementById('five');
+const close5 = document.getElementById('close5');
+const open6 = document.getElementById('open6');
+const six = document.getElementById('six');
+const close6 = document.getElementById('close6');
+const open7 = document.getElementById('open7');
+const seven = document.getElementById('seven');
+const close7 = document.getElementById('close7');
+const open12 = document.getElementById('open12');
+const open22 = document.getElementById('open22');
+const open32 = document.getElementById('open32');
+const open42 = document.getElementById('open42');
+const open52 = document.getElementById('open52');
+const open62 = document.getElementById('open62');
+const open72 = document.getElementById('open72');
+
+open1.addEventListener('click', () => {
+    one.classList.add('show');
+})
+open12.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close1.addEventListener('click', () => {
+    one.classList.remove('show');
+})
+open2.addEventListener('click', () => {
+    two.classList.add('show');
+})
+open22.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close2.addEventListener('click', () => {
+    two.classList.remove('show');
+})
+open3.addEventListener('click', () => {
+    three.classList.add('show');
+})
+open32.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close3.addEventListener('click', () => {
+    three.classList.remove('show');
+})
+open4.addEventListener('click', () => {
+    four.classList.add('show');
+})
+open42.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close4.addEventListener('click', () => {
+    four.classList.remove('show');
+})
+open5.addEventListener('click', () => {
+    five.classList.add('show');
+})
+open52.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close5.addEventListener('click', () => {
+    five.classList.remove('show');
+})
+open6.addEventListener('click', () => {
+    six.classList.add('show');
+})
+open62.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close6.addEventListener('click', () => {
+    six.classList.remove('show');
+})
+open7.addEventListener('click', () => {
+    seven.classList.add('show');
+})
+open72.addEventListener('click', () => {
+    one.classList.add('show');
+})
+close7.addEventListener('click', () => {
+    seven.classList.remove('show');
+})
